b3cc0a93f8120daf3fc5522163a497f0d1d2360f
dvdrtrgn <dvdrtrgn@gmail.com>
2019-01-30 15:15:00 -0600

    Rev syntax

--- generator/template/src/App.vue
+++ generator/template/src/App.vue
@@ -1,6 +1,6 @@
 <template lang='pug'>
-#app
-  img(alt='Vue logo', src='./assets/logo.png')
+#App
+  img(alt='Vue logo' src='./assets/logo.png')
   HelloWorld(msg='Welcome to Your Vue.js App')
 </template>
 
@@ -8,18 +8,20 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld
   }
 }
 </script>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
+<style lang='scss'>
+#App {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- generator/template/src/components/HelloWorld.vue
+++ generator/template/src/components/HelloWorld.vue
@@ -4,39 +4,52 @@
   p
     | For a guide and recipes on how to configure / customize this project,
     br
-    |       check out the
-    a(href='https://cli.vuejs.org', target='_blank', rel='noopener') vue-cli documentation
+    | check out the
+    a(href='https://cli.vuejs.org'
+      target='_blank' rel='noopener') vue-cli documentation
     | .
   h3 Installed CLI Plugins
   ul
     li
-      a(href='https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel', target='_blank', rel='noopener') babel
+      a(href='https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel'
+        target='_blank' rel='noopener') babel
     li
-      a(href='https://www.npmjs.com/package/vue-cli-plugin-pug', target='_blank', rel='noopener') pug
+      a(href='https://www.npmjs.com/package/vue-cli-plugin-pug'
+        target='_blank' rel='noopener') pug
   h3 Essential Links
   ul
     li
-      a(href='https://vuejs.org', target='_blank', rel='noopener') Core Docs
+      a(href='https://vuejs.org'
+        target='_blank' rel='noopener') Core Docs
     li
-      a(href='https://forum.vuejs.org', target='_blank', rel='noopener') Forum
+      a(href='https://forum.vuejs.org'
+        target='_blank' rel='noopener') Forum
     li
-      a(href='https://chat.vuejs.org', target='_blank', rel='noopener') Community Chat
+      a(href='https://chat.vuejs.org'
+        target='_blank' rel='noopener') Community Chat
     li
-      a(href='https://twitter.com/vuejs', target='_blank', rel='noopener') Twitter
+      a(href='https://twitter.com/vuejs'
+        target='_blank' rel='noopener') Twitter
     li
-      a(href='https://news.vuejs.org', target='_blank', rel='noopener') News
+      a(href='https://news.vuejs.org'
+        target='_blank' rel='noopener') News
   h3 Ecosystem
   ul
     li
-      a(href='https://router.vuejs.org', target='_blank', rel='noopener') vue-router
+      a(href='https://router.vuejs.org'
+        target='_blank' rel='noopener') vue-router
     li
-      a(href='https://vuex.vuejs.org', target='_blank', rel='noopener') vuex
+      a(href='https://vuex.vuejs.org'
+        target='_blank' rel='noopener') vuex
     li
-      a(href='https://github.com/vuejs/vue-devtools#vue-devtools', target='_blank', rel='noopener') vue-devtools
+      a(href='https://github.com/vuejs/vue-devtools#vue-devtools'
+        target='_blank' rel='noopener') vue-devtools
     li
-      a(href='https://vue-loader.vuejs.org', target='_blank', rel='noopener') vue-loader
+      a(href='https://vue-loader.vuejs.org'
+        target='_blank' rel='noopener') vue-loader
     li
-      a(href='https://github.com/vuejs/awesome-vue', target='_blank', rel='noopener') awesome-vue
+      a(href='https://github.com/vuejs/awesome-vue'
+        target='_blank' rel='noopener') awesome-vue
 
 </template>
 
@@ -50,18 +63,19 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="stylus">
-h3
-  margin 40px 0 0
-
-ul
-  list-style-type none
-  padding 0
-
-li
-  display inline-block
-  margin 0 10px
-
-a
-  color #42b983
+<style scoped lang='scss'>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
